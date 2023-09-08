7d0908f55d5af94f03497483c546954f845df1d3
NazarenoLorenzatti <nl.loragro@gmail.com>
2023-09-08 15:03:28 -0300

    Autenticacion en dos pasos

--- src/main/java/net/ultrafibra/cotrasenas/controller/LoginController.java
+++ src/main/java/net/ultrafibra/cotrasenas/controller/LoginController.java
@@ -1,30 +1,41 @@
 package net.ultrafibra.cotrasenas.controller;
 
 import net.ultrafibra.cotrasenas.model.Usuario;
-import net.ultrafibra.cotrasenas.response.JWTResponseRest;
+import net.ultrafibra.cotrasenas.response.*;
 import net.ultrafibra.cotrasenas.service.impl.LoginServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class LoginController {
 
     @Autowired
     private LoginServiceImpl loginService;
-    
+
     /**
-     * Login de Usuario
+     * Primer Factor de autenticacion
      * 
      * @param loginRequest
      * @return
      * @throws Exception 
-     */    
+     */
+    @PostMapping(path = "/primer-factor", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseRest> primerFactor(@RequestBody Usuario loginRequest) throws Exception {
+        return loginService.primerFactor(loginRequest.getUsername(), loginRequest.getPassword());
+    }
+
+    /**
+     * Segundo Factor de autenticacion y proporcion de TOKEN
+     *
+     * @param loginRequest
+     * @return
+     * @throws Exception
+     */
     @PostMapping(path = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<JWTResponseRest> login(@RequestBody Usuario loginRequest) throws Exception {
-         return  loginService.login(loginRequest.getUsername(), loginRequest.getPassword());
+        return loginService.login(loginRequest.getUsername(), loginRequest.getPassword(), loginRequest.getPin());
     }
 
 }
--- src/main/java/net/ultrafibra/cotrasenas/service/iLoginService.java
+++ src/main/java/net/ultrafibra/cotrasenas/service/iLoginService.java
@@ -1,11 +1,14 @@
 package net.ultrafibra.cotrasenas.service;
 
 import net.ultrafibra.cotrasenas.response.JWTResponseRest;
+import net.ultrafibra.cotrasenas.response.ResponseRest;
 import org.springframework.http.ResponseEntity;
 
 
 
 public interface iLoginService {
     
-    public ResponseEntity<JWTResponseRest> login(String userName, String password);
+    public ResponseEntity<ResponseRest> primerFactor(String userName, String password);
+    public ResponseEntity<JWTResponseRest> login(String userName, String password, int pin);
+    
 }
